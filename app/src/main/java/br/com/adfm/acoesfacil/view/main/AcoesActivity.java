903d1fd40cbd19ee2c8018065666746fcf1d0da2
ams918 <alexandre.morais918@hotmail>
2015-04-07 01:55:47 -0300

    Alterações
    1- Correções na persistência
    
    2- Classe de consulta ativo via webservice

--- app/src/main/java/br/com/adfm/acoesfacil/view/main/AcoesActivity.java
+++ app/src/main/java/br/com/adfm/acoesfacil/view/main/AcoesActivity.java
@@ -1,7 +1,10 @@
 package br.com.adfm.acoesfacil.view.main;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,6 +23,7 @@ import br.com.adfm.acoesfacil.service.ConsultaAcoesXML;
 
 public class AcoesActivity extends ActionBarActivity {
 
+    private Ativo ativo;
     private ImageButton btnBuscar;
     private ImageView imgFavorito;
     private ImageView imgOscilacao;
@@ -34,6 +38,7 @@ public class AcoesActivity extends ActionBarActivity {
     private TextView lblMinima;
     private TextView lblMaxima;
     private TextView lblData;
+    private ProgressDialog progress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +58,7 @@ public class AcoesActivity extends ActionBarActivity {
         this.lblMinima = (TextView) findViewById(R.id.lblMinima);
         this.lblMaxima= (TextView) findViewById(R.id.lblMaxima);
         this.lblData= (TextView) findViewById(R.id.lblData);
+        this.progress = new ProgressDialog(this);
 
         limparTela();
 
@@ -62,6 +68,7 @@ public class AcoesActivity extends ActionBarActivity {
     }
 
     private void limparTela() {
+        AcoesActivity.this.imgFavorito.setImageResource(R.drawable.ic_acao_favorito);
         this.imgFavorito.setVisibility(View.INVISIBLE);
         this.imgOscilacao.setVisibility(View.INVISIBLE);
 
@@ -73,6 +80,7 @@ public class AcoesActivity extends ActionBarActivity {
         this.lblMedio.setText("");
         this.lblMinima.setText("");
         this.lblMaxima.setText("");
+        this.lblData.setText("  ");
     }
 
     private void addListener() {
@@ -87,41 +95,97 @@ public class AcoesActivity extends ActionBarActivity {
 
                     String link= "http://www.bmfbovespa.com.br/Pregao-Online/ExecutaAcaoAjax.asp?CodigoPapel=" + strAtivo.toUpperCase();
                     AcoesActivity.this.consultaAcoes = new ConsultaAcoesXML(link );
-                    Ativo ativo = AcoesActivity.this.consultaAcoes.consultar();
-                    if (ativo == null) {
-                        Toast.makeText(getApplicationContext(),R.string.acao_msg_ativo_nao_encontrado , Toast.LENGTH_SHORT).show();
-                    } else {
-                        AcoesActivity.this.lblAtivo.setText(ativo.getCodigo());
-
-                        AcoesActivity.this.lblUltimoPreco.setText("R$ " + ativo.getUltimo());
-                        AcoesActivity.this.lblAbertura.setText("Abert R$ " + ativo.getAbertura());
-                        AcoesActivity.this.lblMedio.setText("Médio R$ " + ativo.getMedio());
-                        AcoesActivity.this.lblMinima.setText("Máx. R$ " + ativo.getMinimo());
-                        AcoesActivity.this.lblMaxima.setText("Min R$ " + ativo.getMaximo());
-
-                        AcoesActivity.this.imgOscilacao.setVisibility(View.VISIBLE);
-                        AcoesActivity.this.imgFavorito.setVisibility(View.VISIBLE);
-                        if (ativo.getOscilacao() > 0){
-                            AcoesActivity.this.imgOscilacao.setImageResource(R.drawable.ic_up);
-                        } else {
-                            AcoesActivity.this.imgOscilacao.setImageResource(R.drawable.ic_down);
-                        }
-                        Toast.makeText(getApplicationContext(),R.string.acao_msg_ativo_encontrado , Toast.LENGTH_SHORT).show();
-                    }
+                    progress = new ProgressDialog(AcoesActivity.this);
+                    progress.setMessage("Por favor aguarde");
+                    progress.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+                    progress.show();
+                    new ConsultarAcoesAsynckTask().execute(strAtivo.toUpperCase());
 
                 }
             }
         });
     }
 
+    public void setAtivo(Ativo ativo) {
+        this.ativo = ativo;
+    }
+
+    public Ativo getAtivo() {
+        return ativo;
+    }
+
+    private class ConsultarAcoesAsynckTask extends AsyncTask<String, Void, Ativo>{
+        String link= "http://www.bmfbovespa.com.br/Pregao-Online/ExecutaAcaoAjax.asp?CodigoPapel=";
+        private String strUrl = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected Ativo doInBackground(String... params) {
+            ConsultaAcoes consultaAcoes = new ConsultaAcoesXML(link + params[0].toUpperCase());
+
+            return consultaAcoes.consultar();
+        }
+
+        @Override
+        protected void onPostExecute(Ativo ativo) {
+
+            if (ativo == null || ativo.getCodigo() == null ) {
+                Toast.makeText(getApplicationContext(),R.string.acao_msg_ativo_nao_encontrado , Toast.LENGTH_SHORT).show();
+            } else {
+
+                AcoesActivity.this.lblAtivo.setText(ativo.getCodigo());
+
+                AcoesActivity.this.lblUltimoPreco.setText("R$ " + ativo.getUltimo());
+                AcoesActivity.this.lblAbertura.setText("Abert R$ " + ativo.getAbertura());
+                AcoesActivity.this.lblMedio.setText("Médio R$ " + ativo.getMedio());
+                AcoesActivity.this.lblMinima.setText("Máx. R$ " + ativo.getMinimo());
+                AcoesActivity.this.lblMaxima.setText("Min R$ " + ativo.getMaximo());
+
+                AcoesActivity.this.imgOscilacao.setVisibility(View.VISIBLE);
+                AcoesActivity.this.imgFavorito.setVisibility(View.VISIBLE);
+
+                Ativo ativoTemp = AcoesActivity.this.dao.consultarPeloAtivo(ativo.getCodigo());
+
+                if (ativoTemp != null) {
+                    AcoesActivity.this.imgFavorito.setImageResource(R.drawable.ic_acao_nao_favorita);
+                } else {
+                    AcoesActivity.this.imgFavorito.setImageResource(R.drawable.ic_acao_favorito);
+                }
+
+                if (ativo.getOscilacao() > 0){
+                    AcoesActivity.this.imgOscilacao.setImageResource(R.drawable.ic_up);
+                } else {
+                    AcoesActivity.this.imgOscilacao.setImageResource(R.drawable.ic_down);
+                }
+                Toast.makeText(getApplicationContext(),R.string.acao_msg_ativo_encontrado , Toast.LENGTH_SHORT).show();
+            }
+            AcoesActivity.this.progress.setProgress(100);
+            AcoesActivity.this.progress.dismiss();
+            Log.d("ASYNC Retorno", "");
+        }
+    }
+
     public void favoritarAtivo(View view) {
-        /*if (this.lblAtivo.getText() == null || this.lblAtivo.getText().length() == 0 ){
-
+        if (this.lblAtivo.getText() == null || this.lblAtivo.getText().length() == 0 ){
+            Toast.makeText(getApplicationContext(),R.string.acao_msg_ativo_vazio , Toast.LENGTH_SHORT).show();
         } else {
+            String codigo = txtAtivo.getText().toString();
+            Ativo ativoTemp = this.dao.consultarPeloAtivo(codigo);
 
-        }*/
-        String codigo = txtAtivo.getText().toString();
-        Toast.makeText(getApplicationContext(),"Favoritar ativo" , Toast.LENGTH_SHORT).show();
+            if (ativoTemp != null){
+                this.dao.remover(ativoTemp);
+                this.imgFavorito.setImageResource(R.drawable.ic_acao_nao_favorita);
+                Toast.makeText(getApplicationContext(),R.string.acao_msg_ativo_removido , Toast.LENGTH_SHORT).show();
+            } else{
+                this.dao.adicionar(new Ativo(codigo, 0d,0d));
+                this.imgFavorito.setImageResource(R.drawable.ic_acao_favorito);
+                Toast.makeText(getApplicationContext(),R.string.acao_msg_ativo_favorito , Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 
